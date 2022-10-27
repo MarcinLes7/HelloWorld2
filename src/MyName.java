39675b0889b8dafb179440b41910fd0daf0d4318
MarcinLes7 <Gitm@rtino7>
2022-10-27 16:30:05 +0200

    Poprawa  pliku z imieniem i nazwiskiem

--- src/MyName.java
+++ src/MyName.java
@@ -2,6 +2,6 @@ public class MyName {
     public static void main(String[] args) {
         String name = "Marcin";
         String surname = "Leśniak";
-        System.out.println("Moje imię to: " + name + surname);
+        System.out.println("Moje imię to: " + name + " " + surname);
     }
 }
